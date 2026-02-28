9a07e260a35c36dda1e3e84ea3b2c1af98566955
artur <breadlol64@gmail.com>
2026-02-28 12:46:15 +0200

    phnoe

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,21 @@
+<script>
+	import axios from 'axios';
+	import { onMount } from 'svelte';
+	let level = $state('loading');
+
+	onMount(async () => {
+		try {
+			const response = await axios.get('/api/battery/get');
+			console.log(response);
+			level = response.data;
+		} catch (error) {
+			console.error(error);
+		}
+	});
+</script>
+
 <h1>Welcome to aaaaaa</h1>
+
+<div>
+	my phnoe btarerr level: {level}%
+</div>
--- src/routes/api/battery/get/+server.ts
+++ src/routes/api/battery/get/+server.ts
@@ -2,16 +2,12 @@ import { json } from '@sveltejs/kit';
 import { getStore } from '@netlify/blobs';
 import { NETLIFY_TOKEN } from '$env/static/private';
 
-export async function GET({ params }) {
+export async function GET() {
 	const store = getStore('battery', {
 		siteID: '7a3ce420-7b3d-49c0-a52c-ac525110bb63',
 		token: NETLIFY_TOKEN
 	});
-	if (params.level) {
-		await store.set('level', params.level);
+	const level = await store.get('level');
 
-		return json(await store.get('level'));
-	}
-
-	return json('no level');
+	return json(level);
 }
